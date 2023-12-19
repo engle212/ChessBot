68c314832f2fdbf8e5e4eec4438e46f074c77b9c
engle212 <engle.212@buckeyemail.osu.edu>
2023-12-19 18:19:19 -0500

    Made dataCollect faster

--- dataCollect.py
+++ dataCollect.py
@@ -6,13 +6,15 @@ import pandas as pd
 import numpy as np
 from datetime import date
 
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+
 def getNames(titles):
     nameSet = set()
     for t in titles:
         # Get list of usernames in title t
         url = "https://api.chess.com/pub/titled/" + t
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
-        r = requests.get(url, headers=headers)
+        headers = {'User-Agent': USER_AGENT}
+        r = requests.get(url, headers={'User-Agent': USER_AGENT})
         
         if r.status_code != 200:
             print(r)
@@ -26,7 +28,7 @@ def getNames(titles):
 def getPGNurls(name):
     archives = []
     url = "https://api.chess.com/pub/player/" + str(name) + "/games/archives"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': USER_AGENT}
     r = requests.get(url, headers=headers)
 
     if r.status_code != 200:
@@ -38,7 +40,7 @@ def getPGNurls(name):
 
 def getPGN(url):
     pgn = ''
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 
+    headers = {'User-Agent': USER_AGENT, 
                'Content-Type': 'application/x-chess-pgn',
                'Content-Disposition': 'attachment; filename="ChessCom_username_YYYMM.pgn'}
     r = requests.get(url, headers=headers)
@@ -49,56 +51,76 @@ def getPGN(url):
         pgn = r.text
     return pgn
 
-def getMoves(game):
+def getData(url):
+    pgn = ''
+    versus = ''
     moves = []
-    for move in game.mainline_moves():
-        moves.append(move.uci())
-    # Even moves: white 
-    # Odd moves: black
-    return moves     
+    headers = {'User-Agent': USER_AGENT, 
+               'Content-Type': 'application/x-chess-pgn',
+               'Content-Disposition': 'attachment; filename="ChessCom_username_YYYMM.pgn'}
+    r = requests.get(url, headers=headers)
+
+    if r.status_code != 200:
+        print(r)
+    else:
+        pgn = r.text
+
+        game = chess.pgn.read_game(io.StringIO(pgn))
+
+        if game is not None:
+            versus = '{' + game.headers['White'] + '} vs {' + game.headers['Black'] + '}'
+            moves = [move.uci() for move in game.mainline_moves()]
+    return versus, moves
 
 if __name__ == "__main__":
     #n = getNames(['GM', 'WGM', 'IM'])
-    n = getNames(['IM'])
-    url_list = getPGNurls(n.pop())
-    for name in n:
-        url_list.extend(getPGNurls(name))
+    names = getNames(['GM'])
+    #url_list = getPGNurls(n.pop())
+
+    print('Assembling list of URLs')
+
+    url_list = []
+    for n in names:
+        if len(url_list) < 1000:
+            url_list.extend(getPGNurls(n))
+        else:
+            break
     
     print('Creating DataFrame')
 
     vsList = []
     movesList = []
+
     for url in url_list:
         pgn = getPGN(url)
+        versus = ''
+        moves = []
+
         game = chess.pgn.read_game(io.StringIO(pgn))
 
-        versus = '{' + game.headers['White'] + '} vs {' + game.headers['Black'] + '}'
+        if game is not None:
+            versus = '{' + game.headers['White'] + '} vs {' + game.headers['Black'] + '}'
+            moves = list(map(lambda m: m.uci(), game.end().board().move_stack))
 
-        moves = getMoves(game)
+            vsList.append(versus)
+            movesList.append(moves)
 
-        vsList.append(versus)
-        movesList.append(moves)
-    
-    pgn_df = pd.DataFrame()
+    pgn_df = pd.DataFrame(columns=['WhiteVsBlack', 'Moves'])
     pgn_df['WhiteVsBlack'] = pd.Series(data=vsList)
     pgn_df['Moves'] = pd.Series(data=movesList, dtype='object')
     
     pgn_df.drop_duplicates(subset='WhiteVsBlack', inplace=True)
+    print(pgn_df)
 
     print('DataFrame complete')
 
     print('Exporting data to CSV')
 
     # Export data to csv
-    pgn_df.to_csv('ChessDataAccessed'+ date.today())
+    pgn_df.to_csv('ChessDataAccessed'+ str(date.today())+'.csv')
 
     print('Data successfully exported to CSV')
 
     #gameFile = open("test.pgn", "w", encoding="utf-8")
     #exporter = chess.pgn.FileExporter(gameFile)
     #game_string = game1.accept(exporter)
-
-    
-
-
-
